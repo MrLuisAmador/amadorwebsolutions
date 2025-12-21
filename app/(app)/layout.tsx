77cbeedc9c8b508868f7dcb1e705e056c10cc195
Luis Amador <mrluisamador@gmail.com>
2025-12-21 02:26:02 -0500

    adding Payload CMS to the project

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../../app/globals.css";
 import { Header } from "@/components/layout/header";
 import { Nav } from "@/components/layout/nav";
 import { Footer } from "@/components/layout/footer";
