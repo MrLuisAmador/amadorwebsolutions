2dc42d7a0b7b024c111554954dfeb8048633ef11
Luis Amador <mrluisamador@gmail.com>
2025-12-21 01:38:12 -0500

    adding layout global files

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
 import { Button } from "@/components/ui/button";
+import type { Metadata } from "next";
+
+const siteUrl = "https://www.mrluisamador.com/";
+const siteTitle = "Luis Amador Portfolio";
+const siteDescription =
+  "Luis Amador portfolio to showcase his talents and skills.";
+
+export const metadata: Metadata = {
+  title: `Home | ${siteTitle}`,
+  description: siteDescription,
+  keywords: ["Next.js", "React", "JavaScript"],
+  alternates: {
+    canonical: siteUrl,
+  },
+  openGraph: {
+    title: siteTitle,
+    description: siteDescription,
+    images: ["/images/mugshot.jpg"],
+    url: siteUrl,
+    siteName: siteTitle,
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+};
 
 export default function Home() {
   return (
